49276489fb376e3bbfc09c929814edca4bfe8297
Luuk Schagen <>
2018-03-19 21:42:22 +0200

    Created firebase file and links to gallerycopy

---
+++ public/assets/js/firebase.js
@@ -0,0 +1,12 @@
+var config = {
+  apiKey: "AIzaSyDEhUQ-1nhecJMakgRCbJypQ72tXi5OOJw",
+  authDomain: "luuk-stellenbosch-experience.firebaseapp.com",
+  databaseURL: "https://luuk-stellenbosch-experience.firebaseio.com",
+  projectId: "luuk-stellenbosch-experience",
+  storageBucket: "luuk-stellenbosch-experience.appspot.com",
+  messagingSenderId: "303921280364"
+};
+
+firebase.initializeApp(config);
+
+
