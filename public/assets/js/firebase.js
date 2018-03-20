f09aef640d6e559feccb80c690f721c715278343
Luuk Schagen <>
2018-03-20 15:01:50 +0200

    Storage working for replacing pictures in Gallery (statically)

--- public/assets/js/firebase.js
+++ public/assets/js/firebase.js
@@ -7,6 +7,27 @@ var config = {
   messagingSenderId: "303921280364"
 };
 
-firebase.initializeApp(config);
+//Storage
+var app = firebase.initializeApp(config);
+var storage = app.storage();
+var ref = storage.ref();
+
+var image = ref.child("Gallery").child("IMG_0858_1.JPG");
+
+var webimage = document.getElementsByClassName("image fit");
+
+console.log(webimage)
+
+var imageURL;
+
+image.getDownloadURL().then(function(url) {
+	for (var i = 0; i < webimage.length; i++) {
+		webimage[i].href = url;
+		webimage[i].children[0].src = url;
+	}
+})
+
+
+//end Storage
 
 
