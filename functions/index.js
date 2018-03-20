6187bfe161c6c702d332825173a26b58c399e450
Luuk Schagen <>
2018-03-21 01:09:13 +0200

    Implemented rudimentary string parsing for storage field

--- functions/index.js
+++ functions/index.js
@@ -6,3 +6,28 @@ const functions = require('firebase-functions');
 // exports.helloWorld = functions.https.onRequest((request, response) => {
 //  response.send("Hello from Firebase!");
 // });
+
+
+
+exports.storageWatcher = functions.storage.object().onChange((event) => {
+	var location = event.data.selfLink;
+	console.log(location);
+	var path = [];
+	var string;
+	for (var i = location.length-1; i >= 0; i--) {
+		var char = location[i];
+		console.log(char);
+		if (char === '/'){
+			path.push(string);
+			string = '';
+			console.log(path);
+		}
+		else {
+			string = char + string;
+			console.log(string);
+		}
+	}
+	console.log(path);
+});
+
+
